6fc28c8e2f515902c39578b8e43d70af506d4497
母利睦人 <makomori@makomori-MacBook-Pro.local>
2017-05-11 17:47:28 +0900

    Create project using Cocoapods command

---
+++ Example/Pods/Target Support Files/Dots/Dots-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Dots : NSObject
+@end
+@implementation PodsDummy_Dots
+@end
---
+++ Example/Pods/Target Support Files/Pods-Dots_Tests/Pods-Dots_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dots_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Dots_Tests
+@end
