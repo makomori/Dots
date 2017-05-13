134b8c2fc455e45f2f96a9b94b7c4da0382269a1
makomori <makomori26@gmail.com>
2017-05-13 23:38:03 +0900

    Add dismiss animation of dots

--- Dots/Classes/DotsLoadingView.swift
+++ Dots/Classes/DotsLoadingView.swift
@@ -18,8 +18,8 @@ public class DotsLoadingView: UIView {
     private let myRed = UIColor.hexStr(hexStr: "#F74239", alpha: 1.0)
     private let myYellow = UIColor.hexStr(hexStr: "#FDBC02", alpha: 1.0)
     private let myGreen = UIColor.hexStr(hexStr: "#4AB552", alpha: 1.0)
-    
     public var colors: [UIColor] = []
+    private var dots: [DotView] = []
     
     public init(colors: [UIColor]?) {
         super.init(frame: CGRect(x: 0, y: 0, width: 150, height: 100))
@@ -51,7 +51,20 @@ public class DotsLoadingView: UIView {
     }
     
     public func stop() {
-        self.removeFromSuperview()
+        if #available(iOS 10.0, *) {
+            let animator = UIViewPropertyAnimator(duration: 0.5, curve: .easeInOut) {
+                for dot in self.dots {
+                    dot.alpha = 0
+                }
+            }
+            animator.startAnimation()
+            animator.addCompletion { _ in
+                self.removeFromSuperview()
+            }
+        } else {
+            // Fallback on earlier versions
+            print("It's only availabel from iOS 10")
+        }
     }
     
     private func startAnimation() {
@@ -59,6 +72,7 @@ public class DotsLoadingView: UIView {
             let dotFrame = CGRect(x: 0, y: 0, width: CGFloat(length), height: CGFloat(length))
             let dot = DotView(color: colors[i], delay: self.delay*Double(i), frame: dotFrame)
             dot.center = CGPoint(x: self.frame.width/8 + CGFloat(i)*self.frame.width/4, y: self.frame.height/2)
+            dots.append(dot)
             self.addSubview(dot)
         }
     }
